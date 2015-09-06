064132238feb9d2e2791fe49589f4f652e54fd6c
paresharma <paresh.brahm@gmail.com>
2015-09-06 21:30:11 +0200

    Activate authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'scribbles#index'
   resources :scribbles
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
