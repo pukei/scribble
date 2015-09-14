90c8d0b3cc66dcebb29b0659e22771770a6ecbdc
paresharma <paresh.brahm@gmail.com>
2015-09-14 23:19:40 +0200

    Better this way I guess

--- app/controllers/scribbles_controller.rb
+++ app/controllers/scribbles_controller.rb
@@ -12,7 +12,6 @@ class ScribblesController < ApplicationController
   # GET /scribbles/1
   # GET /scribbles/1.json
   def show
-    @comments = @scribble.comments.order('created_at DESC') if params[:comments] == 'true'
   end
 
   # GET /scribbles/new
@@ -67,7 +66,7 @@ class ScribblesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_scribble
-    @scribble = Scribble.friendly.find(params[:id])
+    @scribble = Scribble.includes(:comments).friendly.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :scribble
 
+  default_scope -> { order('created_at DESC') }
+
   include RenderMarkdown
   def markup
     markdown.render self.comment
