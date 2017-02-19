b55bb904262595d2be3f36879e590dde6a424ca6
paresharma <paresh.brahm@gmail.com>
2017-02-19 20:00:37 +0900

    Remove comments for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'scribbles#index'
   resources :scribbles do
-    resources :comments, only: [:create, :destroy, :update]
+    # resources :comments, only: [:create, :destroy, :update]
   end
 
   get '/login' => 'sessions#new'
